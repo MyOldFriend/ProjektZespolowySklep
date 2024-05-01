2efc1eb2594f8734d6d0e4ed6e7e27e721d96185
AK-Kacper-Adamczyk-26537-PANS <167901623+AK-Kacper-Adamczyk-26537-PANS@users.noreply.github.com>
2024-05-01 20:26:33 +0200

    logowanie-skonczone?

--- src/main/java/com/example/sklep2xd/Repositories/UserRep.java
+++
@@ -1,8 +0,0 @@
-package com.example.sklep2xd.Repositories;
-
-import com.example.sklep2xd.Models.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRep extends JpaRepository<UserEntity, Integer> {
-    UserEntity findByLogin(String Login);
-}
