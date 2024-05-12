b7f6abd3ac6ffa8b70663da47ed67c7f8195f463
MyOldFriend <134224535+MyOldFriend@users.noreply.github.com>
2024-05-12 18:53:51 +0200

    Logowanie Pracowników

--- src/main/java/com/example/sklep2xd/Controllers/AuthController.java
+++ src/main/java/com/example/sklep2xd/Controllers/AuthController.java
@@ -1,22 +1,25 @@
 package com.example.sklep2xd.Controllers;
 
+import com.example.sklep2xd.Dto.LoginDto;
 import com.example.sklep2xd.Dto.RegisterPrDto;
 import com.example.sklep2xd.Models.PracownikEntity;
 import com.example.sklep2xd.Models.RolaPracownika;
 import com.example.sklep2xd.Repositories.PracownikRep;
 import com.example.sklep2xd.Repositories.RolaPracownikaRep;
-import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Security;
 import java.util.Collections;
 
 @Controller
@@ -60,5 +63,12 @@ public class AuthController {
         pracownikRep.save(pracownik);
 
         return new ResponseEntity<>("Pracownik zarejestrowany", HttpStatus.OK);
+    }
+
+    @PostMapping("/logowaniePracownik")
+    public ResponseEntity<String> pracownikLogin(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getLogin(), loginDto.getHaslo()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("Zalogowano pomyślnie", HttpStatus.OK);
     }
 }
